06181767c712d177091c759acee242986ae44e68
CassGabriel <gabriel.rossignoli@cassul.com.br>
2025-01-16 10:46:25 -0300

    Base do site versão 0.1.0

---
+++ src/components/services.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+export const Services = (props) => {
+  return (
+    <div id="services" className="text-center">
+      <div className="container">
+        <div className="section-title">
+          <h2>Nossos Serviços</h2>
+          <p>
+            Serviços de produção e personalização de camisetas com designs exclusivos, alta durabilidade e acabamentos de qualidade para atender a diferentes estilos e necessidades..
+          </p>
+        </div>
+        <div className="row">
+          {props.data
+            ? props.data.map((d, i) => (
+                <div key={`${d.name}-${i}`} className="col-md-4">
+                  {" "}
+                  <i className={d.icon}></i>
+                  <div className="service-desc">
+                    <h3>{d.name}</h3>
+                    <p>{d.text}</p>
+                  </div>
+                </div>
+              ))
+            : "loading"}
+        </div>
+      </div>
+    </div>
+  );
+};
